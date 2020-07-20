38c27276ed3aaf76cf05ca4c8d76e3b2005b17f2
Stephanie <sbowl001@fiu.edu>
2020-07-20 11:44:59 +0800

    ex 5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,8 +7,21 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    found_files = []
+    for file in files: 
+        parts = file.split("/")
+        key = parts[-1]
 
-    return result
+        if key not in cache: 
+            cache[key] = []
+
+        cache[key].append(file)
+
+    for query in queries: 
+        if query in cache: 
+            found_files += cache[query]
+    return found_files
 
 
 if __name__ == "__main__":
