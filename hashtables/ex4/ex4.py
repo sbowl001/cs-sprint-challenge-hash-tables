571b5007e57347b4aa11bab4efd0876f35a27eef
Stephanie <sbowl001@fiu.edu>
2020-07-20 11:05:56 +0800

    try 2 works

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -6,14 +6,23 @@ def has_negatives(a):
 
     cache = {} 
 
-    positive_numbers = list(filter(lambda x: x > 0, a))
+    # positive_numbers = list(filter(lambda x: x > 0, a))
 
-    for x in a: 
-        if -x in positive_numbers:
-            cache[-x] = True 
+    # for x in a: 
+    #     if -x in positive_numbers:
+    #         cache[-x] = True 
     
 
-    return list(cache)
+    # return list(cache)
+
+    # try 2 
+    positive_numbers = [] 
+
+    for x in a: 
+        cache[x] = 1 
+        if x != 0 and x * -1 in cache: 
+            positive_numbers.append(abs(x))
+    return positive_numbers 
 
 
 if __name__ == "__main__":
